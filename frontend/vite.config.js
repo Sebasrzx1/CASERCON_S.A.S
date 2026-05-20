76f4e01e08810c9daa76f28eb2317abea6784d80
Sebasrzx1 <sramirezcarlosama@gmail.com>
2026-05-20 08:45:52 -0500

    modifique el vite.config.js otra vez

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ["reasonable-compassion-production-6024.up.railway.app"],
+    allowedHosts: "all",
   },
 });
